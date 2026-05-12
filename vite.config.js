5c7e780fb797baea042415dca78237c7a82a7a61
Nicolas Gontaro <nicolas.gontaro3@gmail.com>
2026-05-12 10:48:01 -0600

    fix: correct base path to match repo name (double dash)

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+﻿import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  base: "/simulador-hgw-App/",
+  base: "/simulador-hgw--App/",
 
   plugins: [react()],
 
